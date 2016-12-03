d7ff23d36ab3c22d344f6163a739937e2b233dff
joao96 <jvpoletti@gmail.com>
2016-12-03 15:01:49 -0200

    Módulo comentado

--- grafo.c
+++ grafo.c
@@ -22,14 +22,14 @@
 
 p_grafo cria_grafo()
 {
-	p_grafo G;
+	p_grafo G; 
 
-	G = (p_grafo)malloc(sizeof(tp_grafo));
-	assert(G);
+	G = (p_grafo)malloc(sizeof(tp_grafo));// aloca-se o grafo(rede social)
+	assert(G); // checa se foi de fato alocado o espaço para o grafo
 
 
-	G->head = (p_vertice)malloc(sizeof(tp_vertice));
-	assert(G->head);
+	G->head = (p_vertice)malloc(sizeof(tp_vertice)); //cria-se a célula cabeça
+	assert(G->head);// checa se foi de fato alocado o espaço
 	G->ultimo = G->head;
 
 	G->head->prox = NULL;
@@ -53,36 +53,39 @@ void destroi_grafo(p_grafo G)
 	p_vertice v_aux = G->head->prox, v_aux2;
 	p_aresta a_aux, a_aux2;
 
-	assert(G);
+	assert(G); // checa se o grafo de fato existe
 	/*verifica fim da lista de vertices*/
-	while(v_aux != NULL)
+	while(v_aux != NULL) // percorre todos os vértices(usuários) até terminar a lista de vértices
 	{
-		a_aux = v_aux->head->prox;
+		a_aux = v_aux->head->prox; // recebe a lista de arestas desse vértice(lista de amigos desse usuário)
 		/*verifica fim da lista de arestas*/         
 		while(a_aux != NULL){
 			a_aux2 = a_aux;
 			a_aux = a_aux->prox;
-			free(a_aux2);
+			free(a_aux2); // libera essa aresta(amizade)
 		}
-		free(v_aux->head);
+		free(v_aux->head); // libera a célula cabeça desse vértice(usuário)
 		v_aux2 = v_aux;
 		v_aux = v_aux->prox;
-		free(v_aux2);
+		free(v_aux2); // libere o véritce(usuário)
 	}
-	free(G->head);
-	free(G);
+	free(G->head); // libera a célula cabeça do grafo
+	free(G); // libera o grafo
 }
 
 /*******************************************************
 *Função: Adiciona usuário
 *Descrição
 *	Adiciona um usuário a Rede Social.
+*
 *Parâmetros
 *	G - Ponteiro para a estrutura de um grafo.
 *	x - As informações do usuário(tipo estrutura user).
+*
 *Assertiva de entrada
 *	A estrutura do grafo necessita estar inicializada. G não pode possuir o valor NULL.
-*	O nome do usuário não pode conter o caracter '*'.
+*	Nenhum campo de informação do usuário deve conter o caracter '*'.
+*
 *Assertiva de saída
 *	O usuário será inserido na rede social, caso ele ainda não esteja(checa se o nome já existe).
 *********************************************************/
@@ -91,10 +94,10 @@ boolean adiciona_usuario(p_grafo G, tp_user x)
 {
 	p_vertice p = G->head->prox;
 
-	assert(G);
-	if(pesquisa_vertice(G, x.nome) == NULL){
-		p = (p_vertice)malloc(sizeof(tp_vertice));
-		assert(p);
+	assert(G);// checa se o grafo de fato existe
+	if(pesquisa_vertice(G, x.nome) == NULL){ // caso esse usuário não exista(leva-se em consideração o nome fornecido)
+		p = (p_vertice)malloc(sizeof(tp_vertice)); // aloca-se espaço para esse vértice(usuário)
+		assert(p); // checa se foi de fato alocado espaço
 
 		G->ultimo->prox = p;
 		p->ant = G->ultimo;
@@ -103,8 +106,8 @@ boolean adiciona_usuario(p_grafo G, tp_user x)
 
 		p->usuario = x;
 
-		p->head = (p_aresta)malloc(sizeof(tp_aresta));
-		assert(p->head);
+		p->head = (p_aresta)malloc(sizeof(tp_aresta)); // aloca-se a célula cabeça para a sua lista de arestas(amizades)
+		assert(p->head); // checa se foi de fato alocado espaço
 		p->ultimo = p->head;
 		p->head->ant = NULL;
 		p->head->prox = NULL;
@@ -116,6 +119,20 @@ boolean adiciona_usuario(p_grafo G, tp_user x)
 		return FALSE;
 }
 
+/*******************************************************
+*Função: Edita usuário
+*Descrição
+*	Edita um usuário da Rede Social.
+*Parâmetros
+*	user - ponteiro para o usuário que deseja-se editar
+*
+*Assertiva de entrada
+*	o usuário deve existir no grafo.
+*	Nenhum campo de informação do usuário deve conter o caracter '*'.
+*
+*Assertiva de saída
+*	O usuário será inserido na rede social, caso ele ainda não esteja(checa se o nome já existe).
+*********************************************************/
 
 boolean edita_usuario(p_user user)
 {
@@ -124,6 +141,7 @@ boolean edita_usuario(p_user user)
 	int i, j;
 	do
 	{
+		/*Menu para a edição do usuário*/
 		sair = FALSE;
 		system("clear");
 	    printf("***************OPCOES DE EDICAO***********************\n");
@@ -172,7 +190,7 @@ boolean edita_usuario(p_user user)
 				break;
 			
 			case '6':
-				if(user->n_interesses < 20)
+				if(user->n_interesses < 20) // se não atingiu o máximo de interesses possíveis
 				{
 					printf("Digite um interesse: ");
 					scanf("%[^\n]s", user->interesses[user->n_interesses]);
@@ -186,7 +204,7 @@ boolean edita_usuario(p_user user)
 				break;
 			
 			case '7':
-				if(user->n_interesses > 0)
+				if(user->n_interesses > 0) // ele possui algum interesse previamente inserido
 				{
 					printf("Digite o interesse que deseja remover: ");
 					scanf("%[^\n]s", interesse);
@@ -234,24 +252,24 @@ boolean remove_usuario(p_grafo G, char *x)
 	p_vertice v_aux = G->head->prox;
 	p_aresta a_aux = NULL, a_aux2 = NULL;
 
-	assert(G);
+	assert(G); // checa se o grafo já existe
 
-	v_aux = pesquisa_vertice(G, x);
+	v_aux = pesquisa_vertice(G, x); // recebe o ponteiro para o vértice(usuário) que possui esse nome
 
-	if(v_aux == NULL)
+	if(v_aux == NULL) // usuário não existe
 	{
 		return FALSE;
 	}
 	else
 	{	// percorre o grafo removendo a amizade
-		a_aux = v_aux->head->prox;
+		a_aux = v_aux->head->prox; // recebe a lista de amizades(arestas) desse usuário
 		while(a_aux)
 		{
-			a_aux2 = a_aux->amigo->head->prox;
+			a_aux2 = a_aux->amigo->head->prox; // recebe a lista de amizades desse amigo do usuário
 			/*percorrendo amigo e elimando amizade com o usuario*/
 			while(a_aux2)
 			{
-				if(a_aux2->amigo == v_aux)
+				if(a_aux2->amigo == v_aux) // se a amizade foi encontrada
 				{
 					if(a_aux2 == a_aux->amigo->ultimo)
 						a_aux->amigo->ultimo = a_aux2->ant;
@@ -259,7 +277,7 @@ boolean remove_usuario(p_grafo G, char *x)
 						a_aux2->prox->ant = a_aux2->ant;
 					a_aux2->ant->prox = a_aux2->prox;
 
-					free(a_aux2);
+					free(a_aux2); // libera a aresta(amizade)
 					break;
 				}
 				a_aux2 = a_aux2->prox;
@@ -312,35 +330,36 @@ boolean adiciona_amizade(p_grafo G,char *x,char *y)
 	p_vertice v_x = G->head, v_y = NULL;
 	p_aresta a_aux;
 
-	assert(G);
+	assert(G); // checa se o grafo já existe
 
+	/*Checa se os dois usuários, com os nomes fornecidos, existem*/
 	if((v_x = pesquisa_vertice(G, x)) != NULL && (v_y = pesquisa_vertice(G, y)) != NULL)
 		if(pesquisa_aresta(v_x, y) == NULL){
-			/*adicionando em x*/
-			a_aux = (p_aresta)malloc(sizeof(tp_aresta));
-			assert(a_aux);
-			a_aux->ant = v_x->ultimo;
+			/*adicionando a amizade em x*/
+			a_aux = (p_aresta)malloc(sizeof(tp_aresta)); // aloca-se a aresta(amizade)
+			assert(a_aux); // checa se foi alocada
+			a_aux->ant = v_x->ultimo; // faz o encadeamento
 			v_x->ultimo->prox = a_aux;
 			a_aux->prox = NULL;
 
-			v_x->ultimo = a_aux;
+			v_x->ultimo = a_aux; // insere no final da lista de amizades
 			a_aux->amigo = v_y;
 
 			/*adicionando em y*/
-			a_aux = (p_aresta)malloc(sizeof(tp_aresta));
-			assert(a_aux);
-			a_aux->ant = v_y->ultimo;
+			a_aux = (p_aresta)malloc(sizeof(tp_aresta));// aloca-se a aresta(amizade)
+			assert(a_aux);// checa se foi alocada
+			a_aux->ant = v_y->ultimo;// faz o encadeamento
 			v_y->ultimo->prox = a_aux;
 			a_aux->prox = NULL;
 
-			v_y->ultimo = a_aux;
+			v_y->ultimo = a_aux;// insere no final da lista de amizades
 			a_aux->amigo = v_x;
 			
 			return TRUE;
 		}
-		else
+		else // não existe amizade entre os 2 usuários
 			return FALSE;
-	else
+	else// algum usuário inserido não existe
 		return FALSE;
 }
 
@@ -363,9 +382,11 @@ boolean remove_amizade(p_grafo G,char *x, char *y)
 	p_vertice v_x = NULL, v_y = NULL;
 	p_aresta a_aux;
 
-	assert(G);
+	assert(G); // checa se o grafo já existe
 
+	/*Checa se os dois usuários, com os nomes fornecidos, existem*/
 	if((v_x = pesquisa_vertice(G, x)) != NULL && (v_y = pesquisa_vertice(G, y)) != NULL){
+		/*Checa se existe amizade entre esses 2 usuários*/
 		if((a_aux = pesquisa_aresta(v_x, y)) != NULL){
 			if(a_aux == v_x->ultimo)
 				v_x->ultimo = a_aux->ant;
@@ -376,7 +397,7 @@ boolean remove_amizade(p_grafo G,char *x, char *y)
 
 			free(a_aux);
 		}else
-			return FALSE;
+			return FALSE;// não existe amizade
 		if((a_aux = pesquisa_aresta(v_y, x)) != NULL){
 			if(a_aux == v_y->ultimo)
 				v_y->ultimo = a_aux->ant;
@@ -389,7 +410,7 @@ boolean remove_amizade(p_grafo G,char *x, char *y)
 		}
 		return TRUE;
 	}
-	else
+	else // algum dos 2 usuários inseridos não existe
 		return FALSE;
 } 
 
@@ -412,7 +433,7 @@ boolean remove_amizade(p_grafo G,char *x, char *y)
 *********************************************************/
 boolean grafo_vazio(p_grafo G)
 {
-	if(G->head == G->ultimo)
+	if(G->head == G->ultimo) // se a célula cabeça é o fim do grafo
 		return TRUE;
 	else 
 		return FALSE;
@@ -433,7 +454,7 @@ boolean grafo_vazio(p_grafo G)
 *********************************************************/
 boolean vertice_vazio(p_vertice A)
 {
-	if(A->head == A->ultimo)
+	if(A->head == A->ultimo) // se a célula cabeça é o fim do grafo
 		return TRUE;
 	else
 		return FALSE;
@@ -459,18 +480,19 @@ p_vertice pesquisa_vertice (p_grafo G, char *x){
 
 	assert(G);
 
+	/*Se o grafo não está vazio*/
 	if(grafo_vazio(G) == FALSE){
-		while(p != NULL){
-			if(strcmp(x, p->usuario.nome) == 0)
+		while(p != NULL){ // enquanto não chegar no final da lista de usuários
+			if(strcmp(x, p->usuario.nome) == 0) // compara o nome, se for igual ao do inserido via terminal
 				break;
 			p = p->prox;
 		}
 		if(p != NULL)
-			return p;
+			return p; // retorna o ponteiro para esse usuário
 		else
 			return NULL;
 	}
-	else
+	else // grafo não possui vértices(usuários)
 	{
 		return NULL;
 	}
@@ -496,17 +518,17 @@ p_vertice pesquisa_vertice (p_grafo G, char *x){
 p_aresta pesquisa_aresta(p_vertice  V, char *x){
 	p_aresta p = V->head->prox;
 
-	assert(V);
+	assert(V);// checa se o vértice existe(foi alocado da forma correta)
 	assert(x);
 
-	if(vertice_vazio(V) == FALSE){
+	if(vertice_vazio(V) == FALSE){ // se o usuário possuir amizades
 		while(p != NULL){
-			if(strcmp(x, p->amigo->usuario.nome) == 0)
+			if(strcmp(x, p->amigo->usuario.nome) == 0) // percorre sua lista de amizades até encontrar
 				break;
 			p = p->prox;
 		}
-		if(p != NULL)
-			return p;
+		if(p != NULL) // se encontrou
+			return p; // retorna ponteiro para esse amigo
 		else
 			return NULL;
 	}
@@ -531,16 +553,25 @@ boolean imprime_grafo(p_grafo G)
 {
 	p_vertice v_aux = G->head->prox;
 	p_aresta a_aux;
+	int i;
 
-	while(v_aux)
+	while(v_aux) // enquanto não chegar no final da lista de usuários
 	{
-		printf("%s: %p | ant: %p | prox: %p\n", v_aux->usuario.nome,v_aux,v_aux->ant,v_aux->prox);
+		// imprime o nome e o endereço desse usuário, bem como o endereço do usuário anteror e posterior
+		printf("%s: %p | ant: %p | prox: %p\n", v_aux->usuario.nome,v_aux,v_aux->ant,v_aux->prox); 
 		a_aux = v_aux->head->prox;
+		// imprime as informações deste usuário(genero, idade, escolaridade, cep, interesses)
 		printf("\t%c\n", v_aux->usuario.genero);
 		printf("\t%d\n", v_aux->usuario.idade);
 		printf("\t%d\n", v_aux->usuario.escolaridade);
 		printf("\t%d\n", v_aux->usuario.cep);
-
+		printf("\tInteresses:\n");
+		for(i=0;i<v->aux->usuario.n_interesses;i++)
+		{
+			printf("\t");
+			puts(user->usuario.interesses[i]);
+		}
+		// imprime o endereço da lista de amizades 
 		printf("\t\tHEAD: %p | ant: %p | prox: %p\n", v_aux->head,v_aux->head->ant,v_aux->head->prox);
 		while(a_aux)
 		{
@@ -572,7 +603,6 @@ boolean salva_grafo(p_grafo G)
 	arq = fopen("user.txt", "w");
 	while(v)
 	{
-		printf("strlen = %ld\n", strlen(v->usuario.nome));
 		fprintf(arq, ".");
 		fprintf(arq, "%ld", strlen(v->usuario.nome));
 		for(int i=0;i<strlen(v->usuario.nome); i++)
